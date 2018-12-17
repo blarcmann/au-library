c876d0f0cd29d4ddd7cf21814f57874f50de49e4
yemiOdetola <yemiotola@outlook.com>
2018-12-17 12:11:22 +0100

    Setup: Early birds && Env Settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuFaInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
