0c3a027f5b6eb9313fadd1fb3aa49717ebf7b940
yemiOdetola <yemiotola@outlook.com>
2018-12-29 20:28:08 +0100

    Testing: Integrated testing for form components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,49 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FaInputComponent } from './lib/fa-input/fa-input.component';
+import { InputRefDirective } from './lib/common/input-ref.directive';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+
 describe('AppComponent', () => {
+
+  let component: AppComponent,
+    fixture: ComponentFixture<AppComponent>,
+    el: DebugElement,
+    emailField: DebugElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        FaInputComponent,
+        InputRefDirective
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'au-library'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('au-library');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+    el = fixture.debugElement;
+    emailField = el.query(By.css('#email-field'));
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to au-library!');
+  })
+
+
+  it('should create the app', async(() => {
+    expect(component).toBeTruthy();
   }));
+
+  it(`should create a font awesome email input`, async(() => {
+    expect(emailField).toBeTruthy();
+  }))
+  it(`should include the correct icon as specified`, async(() => {
+    console.log(emailField.nativeElement.outerHTML);
+    expect(emailField.query(By.css('i.fa-icon.fa.fa-envelope'))).toBeTruthy();
+  }))
+  it(`should have projected the correct test input inside the assigned field`, async(() => {
+    expect(emailField.query(By.css('input.test-class'))).toBeTruthy();
+  }))
 });
